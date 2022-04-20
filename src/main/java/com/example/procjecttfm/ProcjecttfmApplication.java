9ff166f182c62ae93a5fb3914f67547b211f1e2c
karimbasly <karimbasli@live.fr>
2022-04-20 03:55:40 +0200

    Initial commit

---
+++ src/main/java/com/example/procjecttfm/ProcjecttfmApplication.java
@@ -0,0 +1,13 @@
+package com.example.procjecttfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcjecttfmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcjecttfmApplication.class, args);
+    }
+
+}
